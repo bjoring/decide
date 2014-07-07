77009912928384f583e3f5347eaec03da3146ac8
Tyler Robbins <tdr5wc@virginia.edu>
2014-07-07 09:59:41 -0400

    more work on gng and toolkit

--- experiments/toolkit.js
+++ experiments/toolkit.js
@@ -3,152 +3,201 @@ var io = require('socket.io-client');
 var pub = io.connect("http://localhost:8000/PUB");
 var req = io.connect("http://localhost:8000/REQ");
 
-function aplayer(what) { //TODO: more sound functionality?
-	req.emit("msg", {
-		req: "change-state",
-                addr: "aplayer",
-                data: { stimulus: what,
-			playing: true }
-        });
+function aplayer(what) {
+	return {
+		play: function() {
+				req.emit("msg", {
+					req: "change-state",
+					addr: "aplayer",
+					data: {
+						stimulus: what,
+						playing: true
+					}
+				});
+				pub.on("msg",function (msg) {
+						if (msg.event == "state-changed" && msg.data.playing == false) {
+							callback();
+						}
+					});
+				}
+		// TODO: more functions?
+	}
 }
 
-
-function cue(which, callback) {
-        var togglestate;
+function cue(which) {
+	var togglestate;
 	var timer;
-        return {
-                on: function(duration, callback) {
-                        write_led(which, true);
-                        if (duration) setTimeout(function(){cue(which).off();}, duration);
-                	callback();
-		},
-                off: function(duration, callback) {
-                        write_led(which, false);
+	return {
+		on: function (duration, callback) {
+			write_led(which, true);
+			if (duration) setTimeout(function () {
+				cue(which).off();
+			}, duration); // if
+			callback();
+		}, // return
+		off: function (duration, callback) {
+			write_led(which, false);
 			clearInterval(timer);
-                        if (duration) setTimeout(function(){cue(which).on();}, duration);
-                	callback();
+			if (duration) setTimeout(function () {
+				cue(which).on();
+			}, duration);
+			callback();
 		},
-                blink: function(duration, interval) {
-                        var inter = interval ? interval : 100;
-                        timer = setInterval(toggle, inter);
-                        if (duration) setTimeout(function(){cue(which).off();}, duration);
-                	callback();
-		}               
-        };
-        function toggle() {
-                togglestate = togglestate ? false : true;
-                write_led(which, togglestate);
-        }
+		blink: function (duration, interval, callback) {
+			timer = setInterval(toggle, interval);
+			if (duration) setTimeout(function () {
+				cue(which).off();
+			}, duration);
+			callback();
+		}
+	};
+	function toggle() {
+		togglestate = togglestate ? false: true;
+		write_led(which, togglestate);
+	}
 }
 
-function feed(which, duration, callback){
-        write_feed(which, true);
-        setTimeout(function(){write_feed(which, false);},duration);
+function feed(which, duration, callback) {
+	write_feed(which, true);
+	setTimeout(function () {
+		write_feed(which, false);
+	}, duration);
 	callback();
 }
- 
-function lights(callback) {
-        var which = "house_lights";
-        return {
-                man_set: function(brightness) {
-                        write_led(which, brightness);
-                	callback();
+
+function lights() {
+	var which = "house_lights";
+	return {
+		man_set: function (brightness, callback) {
+			write_led(which, brightness);
+			callback();
 		},
-                clock_set: function(){
-                         req.emit("msg", {
-                                req: "change-state",
-                                addr: "house_lights",
-                                data: { clock_on: true }
-                         });
-			if(callback) callback();
-                },
-		clock_set_off: function() {
+		clock_set: function (callback) {
 			req.emit("msg", {
 				req: "change-state",
-				addr: "houselights",
-				data: {clock_on: false}
+				addr: "house_lights",
+				data: {
+					clock_on: true
+				}
 			});
 			callback();
 		},
-                on: function(duration) {
-                        write_led(which, 255);
-                        if (duration) setTimeout(function(){lights.off();}, duration);
-                	callback();
+		clock_set_off: function (callback) {
+			req.emit("msg", {
+				req: "change-state",
+				addr: "houselights",
+				data: {
+					clock_on: false
+				}
+			});
+			callback();
 		},
-                off: function(duration) {
-                        write_led(which, 0);
-                        if (duration) setTimeout(function(){lights.on();}, duration);
-                	callback();
+		on: function (duration, callback) {
+			write_led(which, 255);
+			if (duration) setTimeout(function () {
+				lights.off();
+			}, duration);
+			callback();
+		},
+		off: function (duration, callback) {
+			write_led(which, 0);
+			if (duration) setTimeout(function () {
+				lights.on();
+			}, duration);
+			callback();
 		}
-        };
+	};
 }
 
-
-
-function keys() {	
+function keys(target) {
 	var response;
 	var correct;
 	var timer;
 	return {
-		response_window:function (target, duration, callback) {
+		response_window:function (duration, callback) {
 			timer = setInterval(report, duration);
-			pub.on("msg", function(msg) {
+			pub.on("msg", function (msg) {
 				if (msg.event == "state-changed" && msg.addr == "keys") {
 					pub.removeListener("msg");
 					clearInterval(timer);
 					var rep = Object.keys(msg.data);
-					response = {time: msg.time, response: rep[0]};
- 					if (target == "none" || target ==  null || !msg.data[target]) correct = false;
+					response = {
+						time: msg.time, response: rep[0]
+					};
+					if (target == "none" || target == null || !msg.data[target]) correct = false;
 					else if (msg.data[target]) correct = true;
 					report();
 				}
-			});	
+			});
 			function report() {
 				if (response) {
-					if(target == "none" || target == null) correct = false;
+					if (target == "none" || target == null) correct = false;
 				} else {
 					if (target == "none" || target == null) correct = true;
-					else { 
+					else {
 						correct = false;
 						var now = Date.now();
-						response ={time: now, response: null} 
+						response = {
+							time: now, response: null
+						}
 					}
-				} 
+				}
 				response.correct = correct;
 				return callback(response);
 			}
 		},
-		wait_for: function(target, repeat, callback){
-			pub.on("msg", function(msg) {
+		wait_for: function (repeat, callback) {
+			pub.on("msg", function (msg) {
 				if (msg.event == "state-changed" && msg.data[target]) {
 					if (repeat == false) pub.removeListener("msg");
 					var rep = Object.keys(msg.data);
-					var data = {time: msg.time, response: rep[0], target: target};
+					var data = {
+						time: msg.time, response: rep[0], target: target
+					};
 					callback(data);
-					}
-				});
+				}
+			});
 		}
 	};
 }
-function write_led(which, state) {	
+function write_led(which, state) {
 	req.emit("msg", {
-               	req: "change-state",
-               	addr: which == "house_lights" ? which : "cue_"+which,
-               	data: { brightness: state }
+		req: "change-state",
+		addr: which == "house_lights" ? which : "cue_" + which,
+		data: {
+			brightness: state
+		}
 	});
 }
 
 function write_feed(which, state) {
-        req.emit("msg", {
-                req: "change-state",
-                addr: "feeder_"+which,
-                data: { feeding: state}
-        });
+	req.emit("msg", {
+		req: "change-state",
+		addr: "feeder_" + which,
+		data: {
+			feeding: state
+		}
+	});
 }
+
+// state manipulation sketch
+function init(name, object) {
+	// create component
+	// set state stuff
+}
+
+function change_state(key, change) {
+	// set value of state stuff
+}
+
+
+
+
+
 module.exports = {
 	cue: cue,
 	feed: feed,
 	lights: lights,
 	keys: keys,
-	aplayer: aplayer	
+	aplayer: aplayer
 };
--- gng.js
+++ gng.js
@@ -1,21 +1,57 @@
-var t = require(./toolkit);
+// Import required modules
+var t = require("./toolkit");
 var winston = require(winston); // logger
 
-function trial(set_stim) {
-	var stim = set_stim ? set_stim :  select_stimuli();
-	keys().wait_for("peck_center", false, function() {
-		present_stimulus();
-	}
-	t.aplayer(stim.sound);
 
-	function select_stimuli() {
+// Parameters
+var trial_count = 0;
+var stim_set = {};
+var correction_count
+var correction_trial;
+
+// Set global variables
+function trial() {
+
+	prep_trial();
+
+	function prep_trial(force_stim) {
+		trial_count++;
+		var stim = force_stim ? force_stim : select_stimulus();
+		keys().wait_for(target_key, false, play_stimulus);
+	}
+
+	function select_stimulus() {
 		var length = Object.keys(stimulus_set).length - 1;
 		var select = Math.round(rand*length);
-		return stimset[select];
+		return stim_set[select];
 	}
 
-	function present_stimulus {
-		t.aplayer(stim.sound);
-						
-	} 
+	function play_stimulus() {
+		t.aplayer(stim.sound, response_check);
+	}
+
+	function response_check() {
+		keys(target_key).response_window(response_time, response_reaction);
+	}
+
+	function response_reaction(result) {
+		if (result.correct) {
+			feed(default_feed, feed_time);
+			return;
+		}
+		else {
+			if (result.response) lights().off(punish_time, run_correction_trial);
+			else run_correction_trial();
+		}
+	}
+
+	function run_correction_trial() {
+		if (!correction_trial) correction_count = 0;
+		correction_trial = true;
+		correction_count++;
+		if (corrections <= correction_limit) {
+			prep_trial(stim);
+		}
+		else return;
+	}
 }
