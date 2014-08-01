ae9c9565258eafdd1b7ab09a39ed3fc56c854bca
Dan Meliza <cdm8j@virginia.edu>
2014-08-01 16:46:00 -0400

    start work on host/controller protocol refactor

--- bbb-server.js
+++ bbb-server.js
@@ -1,264 +1,270 @@
-// this is the broker process for the controller
-var fs = require("fs");
+// this is the broker process for the device
 var _ = require("underscore");
+var fs = require("fs");
+var winston = require("winston");
 var express = require("express");
-var winston = require("winston"); // logger
+var http = require("http");
+var sockets = require("socket.io");
+var sock_cli = require("socket.io-client");
 var apparatus = require("./lib/apparatus");
-var util = require(__dirname + "/lib/util");
+var util = require("./lib/util");
 
 // Log pub and req events at info level. TODO disable after debugging?
 winston.levels.pub = 3;
 winston.levels.req = 3;
 
-var app = require("express")();
-var server = require("http")
-	.Server(app);
+var host_par = JSON.parse(fs.readFileSync(__dirname + "/host-config.json"));
+
+// http/websockets server
+var app = express();
+var server = http.Server(app);
+var io = sockets.listen(server);
+
+// host connection
+var host_addr = "http://" + host_par.addr_int + ":" + host_par.port_int + "/PUB";
+var pubh = sock_cli.connect(host_addr);
+
+pubh.on("connection", function() {
+    winston.info("connected to host socket %s", host_addr);
+});
+
+pubh.on("disconnect", function() {
+    winston.info("lost connection to host (now queuing messages)");
+    host_forward = false;
+});
 
 function controller(params) {
 
-	var meta = {
-		type: "controller"
-	};
+    var meta = {
+        type: "controller"
+    };
 
-	var par = {
-		port: 8000,
-		exp_dir: __dirname + "/experiments/",
-		mail_list: "robbinsdart@gmail.com",
-		host: "mino",
-		host_port: 8010
-	};
+    var par = {
+        mail_list: "robbinsdart@gmail.com",
+        host: "mino",
+        host_port: 8010
+    };
 
-	util.update(par, params);
+    util.update(par, params);
 
-	var exp; // will hold the experiment child process
-
-	var io = require("socket.io").listen(server);
-
-	var state = {
-		host_name: null,
-		online: false,
-		ip_address: null,
-		protocol: null
-	};
-
-	state.host_name = require('os').hostname();
-
-	server.listen(par.port, function() {
-		state.online = true;
-		state.ip_address = util.getIPAddress();
-		winston.info("server listening on", state.ip_address);
-	});
-
-	var pubc = io.of("/PUB");
-	var reqc = io.of("/REQ");
-
-	// host server interaction
-	var clientio = require('socket.io-client');
-	var host_pub = clientio.connect('http://'+par.host+'.local:'+par.host_port+'/BPUB');
-	host_pub.on("connection", function() {
-		winston.info("connected to host");
-	});
-
-	host_pub.on("disconnect", function() {
-		winston.info("disconnected from host");
-		host_forward = false;
-		winston.info("storing messages for next connect");
-	});
-
-	host_pub.on("register-box", function(reply) {
-		reply(state.host_name, state.ip_address, par.port);
-		winston.info(state.host_name, "registered in host");
-		send_store();
-	});
-
-	host_pub.on("send-store", function() {
-		send_store();
-	});
-
-	var msg_store = [];			// holds msg received while disconnected from host-server
-	var host_forward = false;	// whether msgs should be directly forwarded to host-server
-
-	function send_store() {
-		winston.info("sending log store");
-		while (msg_store.length !== 0) {
-			var msg = msg_store.shift();
-			host_pub.emit("msg", msg);
-		}
-
-		// directly forward messages only after mst_store is empty
-		host_forward = true;
-	}
-
-	function prepare_host_disconnect() {
-		// tell host-server not to send an email upon this disconnect
-		host_pub.emit("graceful-exit");
-	}
+    var exp; // will hold the experiment child process
 
 
-	// these methods are called by the apparatus module
-	this.pub = function(msg) {
-		// forward to connected clients
-		pubc.emit("msg", msg);
-		// forward to host
-		if (host_forward) host_pub.emit("msg", msg);
-		else {
-			msg_store.push(msg);
-		}
-	};
+    var state = {
+        host_name: null,
+        online: false,
+        ip_address: null,
+        protocol: null
+    };
 
-	// no requests expected from apparatus
-	this.req = function(msg, rep) {
-		var ret = {};
-		if (msg.req == "get-state") {
-			ret = state;
-		}
-		rep(null, ret);
-	};
+    state.host_name = require("os").hostname();
 
-	// forward pub to connected clients and apparatus components
-	pubc.on("connection", function(socket) {
-		winston.info("connection on PUB:", socket.handshake.address);
-		socket.on("msg", function(msg) {
-			apparatus.pub(msg);
-			socket.broadcast.emit("msg", msg);
-		});
+    server.listen(par.port, function() {
+        state.online = true;
+        state.ip_address = util.getIPAddress();
+        winston.info("server listening on", state.ip_address);
+    });
 
-		socket.on("disconnect", function() {
-			winston.info("disconnect on PUB:", socket.handshake.address);
-		});
-	});
+    var pubc = io.of("/PUB");
+    var reqc = io.of("/REQ");
 
-	// route req to apparatus
-	reqc.on("connection", function(socket) {
-		winston.info("connection on REQ:", socket.handshake.address);
-		socket.on("msg", function(msg, rep) {
-			rep = rep || function() {};
-			apparatus.req(msg, function(err, data) {
-				if (err)
-					rep("req-err", err);
-				else
-					rep("req-ok", data);
-			});
-		});
 
-		socket.on("route", function(key, rep) {
-			// register a client in the apparatus
-			apparatus.register(key, {
-				// client gets pubs through socket already
-				pub: function() {},
-				req: function(msg, rep) {
-					socket.emit("msg", msg, _.partial(rep, null));
-				}
-			});
-			socket.apparatus_key = key;
-			// TODO clients asking for same name?
-			rep("route-ok");
-		});
+    pubh.on("register-box", function(reply) {
+        reply(state.host_name, state.ip_address, par.port);
+        winston.info(state.host_name, "registered in host");
+        send_store();
+    });
 
-		socket.on("unroute", function(key, rep) {
-			apparatus.unregister(key);
-			socket.apparatus_key = undefined;
-			rep("unroute-ok");
-		});
+    pubh.on("send-store", function() {
+        send_store();
+    });
 
-		socket.on("hugz", function(data, rep) {
-			rep("hugz-ok");
-		});
+    // holds msg received while disconnected from host-server
+    var msg_store = [];
+    // whether msgs should be directly forwarded to host-server
+    var host_forward = false;
 
-		socket.on("runexp", function(inexp, args, opt, callback) {
-		  // Spawns an experiment process after error handling
-		  var rep;
-			// check if experiment is not already running
-			if (!exp) {
-				var path = par.exp_dir + inexp;
-				// check if experiment file exists
-				if (fs.existsSync(path) || fs.existsSync(path + ".js")) {
-					if (args[0]) {
-						for (var i = 0; i < args.length; i++) {
-							if (args[i] == "-c") {
-								var argpath = par.exp_dir + args[i + 1];
-								// if given, check for config file's existence
-								if (fs.existsSync(argpath) || fs.existsSync(argpath + ".js") || fs.existsSync(argpath + ".json")) {
-									exp = require('child_process')
-										.fork(path, args, opt);
-									rep = "Running " + inexp;
-								} else {
-									rep = "Error: No such config file";
-								}
-								i = args.length;
-							}
-						}
-					} else {
-						exp = require('child_process')
-							.fork(path, args, opt);
-						rep = "Running " + inexp;
-					}
-				} else {
-					rep = "Error: No such experiment";
-				}
-			} else rep = "Error: Experiment already running.";
-			if (callback) callback(rep);
-		});
+    function send_store() {
+        winston.info("sending log store");
+        while (msg_store.length !== 0) {
+            var msg = msg_store.shift();
+            pubh.emit("msg", msg);
+        }
 
-		socket.on("killexp", function(callback) {
-			if (exp) {
-				exp.kill('SIGINT');
-				exp = null;
-				callback("Experiment stopped.");
-			} else {
-				callback("Error: No experiment to stop!");
-			}
-		});
+        // directly forward messages only after mst_store is empty
+        host_forward = true;
+    }
 
-		socket.on("disconnect", function() {
-			if (socket.apparatus_key)
-				apparatus.unregister(socket.apparatus_key);
-			winston.info("disconnect on REQ:", socket.handshake.address);
-		});
-	});
-	process.on('SIGINT', function() {
-		console.log('SIGINT received.');
-		console.log('Informing host of graceful exit');
-		prepare_host_disconnect();
-		console.log('Exiting gracefully');
-		process.kill();
-	});
+    function prepare_host_disconnect() {
+        // tell host-server not to send an email upon this disconnect
+        pubh.emit("graceful-exit");
+    }
+
+
+    // these methods are called by the apparatus module
+    this.pub = function(msg) {
+        // forward to connected clients
+        pubc.emit("msg", msg);
+        // forward to host
+        if (host_forward) pubh.emit("msg", msg);
+        else {
+            msg_store.push(msg);
+        }
+    };
+
+    // no requests expected from apparatus
+    this.req = function(msg, rep) {
+        var ret = {};
+        if (msg.req == "get-state") {
+            ret = state;
+        }
+        rep(null, ret);
+    };
+
+    // forward pub to connected clients and apparatus components
+    pubc.on("connection", function(socket) {
+        winston.info("connection on PUB:", socket.handshake.address);
+        socket.on("msg", function(msg) {
+            apparatus.pub(msg);
+            socket.broadcast.emit("msg", msg);
+        });
+
+        socket.on("disconnect", function() {
+            winston.info("disconnect on PUB:", socket.handshake.address);
+        });
+    });
+
+    // route req to apparatus
+    reqc.on("connection", function(socket) {
+        winston.info("connection on REQ:", socket.handshake.address);
+        socket.on("msg", function(msg, rep) {
+            rep = rep || function() {};
+            apparatus.req(msg, function(err, data) {
+                if (err)
+                    rep("req-err", err);
+                else
+                    rep("req-ok", data);
+            });
+        });
+
+        socket.on("route", function(key, rep) {
+            // register a client in the apparatus
+            apparatus.register(key, {
+                // client gets pubs through socket already
+                pub: function() {},
+                req: function(msg, rep) {
+                    socket.emit("msg", msg, _.partial(rep, null));
+                }
+            });
+            socket.apparatus_key = key;
+            // TODO clients asking for same name?
+            rep("route-ok");
+        });
+
+        socket.on("unroute", function(key, rep) {
+            apparatus.unregister(key);
+            socket.apparatus_key = undefined;
+            rep("unroute-ok");
+        });
+
+        socket.on("hugz", function(data, rep) {
+            rep("hugz-ok");
+        });
+
+        socket.on("runexp", function(inexp, args, opt, callback) {
+            // Spawns an experiment process after error handling
+            var rep;
+            // check if experiment is not already running
+            if (!exp) {
+                var path = par.exp_dir + inexp;
+                // check if experiment file exists
+                if (fs.existsSync(path) || fs.existsSync(path + ".js")) {
+                    if (args[0]) {
+                        for (var i = 0; i < args.length; i++) {
+                            if (args[i] == "-c") {
+                                var argpath = par.exp_dir + args[i + 1];
+                                // if given, check for config file's existence
+                                if (fs.existsSync(argpath) || fs.existsSync(argpath + ".js") || fs.existsSync(argpath + ".json")) {
+                                    exp = require("child_process")
+                                        .fork(path, args, opt);
+                                    rep = "Running " + inexp;
+                                } else {
+                                    rep = "Error: No such config file";
+                                }
+                                i = args.length;
+                            }
+                        }
+                    } else {
+                        exp = require("child_process")
+                            .fork(path, args, opt);
+                        rep = "Running " + inexp;
+                    }
+                } else {
+                    rep = "Error: No such experiment";
+                }
+            } else rep = "Error: Experiment already running.";
+            if (callback) callback(rep);
+        });
+
+        socket.on("killexp", function(callback) {
+            if (exp) {
+                exp.kill("SIGINT");
+                exp = null;
+                callback("Experiment stopped.");
+            } else {
+                callback("Error: No experiment to stop!");
+            }
+        });
+
+        socket.on("disconnect", function() {
+            if (socket.apparatus_key)
+                apparatus.unregister(socket.apparatus_key);
+            winston.info("disconnect on REQ:", socket.handshake.address);
+        });
+    });
+    process.on("SIGINT", function() {
+        console.log("SIGINT received.");
+        console.log("Informing host of graceful exit");
+        prepare_host_disconnect();
+        console.log("Exiting gracefully");
+        process.kill();
+    });
 }
 
 // TODO be smarter about this if run as script
 var params = {};
 if (process.argv.length > 3) {
-	var buf = fs.readFileSync(process.argv[2]);
-	params = JSON.parse(buf);
+    var buf = fs.readFileSync(process.argv[2]);
+    params = JSON.parse(buf);
 }
 
 // handle http requests from clients
 app.get("/", function(req, res) {
-	res.sendfile(__dirname + "/static/interface.html");
+    res.sendfile(__dirname + "/static/interface.html");
 });
 
 app.get("/state", function(req, res) {
-	apparatus.req({
-		req: "get-state",
-		addr: ""
-	}, function(err, rep) {
-		if (err)
-			res.send(500, err);
-		else
-			res.send(rep);
-	});
+    apparatus.req({
+        req: "get-state",
+        addr: ""
+    }, function(err, rep) {
+        if (err)
+            res.send(500, err);
+        else
+            res.send(rep);
+    });
 });
 
 app.get("/components", function(req, res) {
-	apparatus.req({
-		req: "get-meta",
-		addr: ""
-	}, function(err, rep) {
-		if (err)
-			res.send(500, err);
-		else
-			res.send(rep);
-	});
+    apparatus.req({
+        req: "get-meta",
+        addr: ""
+    }, function(err, rep) {
+        if (err)
+            res.send(500, err);
+        else
+            res.send(rep);
+    });
 });
 
 // all other static content resides in /static
@@ -270,9 +276,9 @@ apparatus.init(params);
 // start the broker
 apparatus.register("controller", new controller(params.controller));
 
-process.on('uncaughtException', function(err) {
-	var subject = "Caught Exception - " + Date.now();
-	var message = err;
-	util.mail("controller", par.mail_list, subject, message, process.exit);
-	console.log(subject);
+process.on("uncaughtException", function(err) {
+    var subject = "Caught Exception - " + Date.now();
+    var message = err;
+    util.mail("controller", par.mail_list, subject, message, process.exit);
+    console.log(subject);
 });
--- host-server.js
+++ host-server.js
@@ -1,169 +1,171 @@
-var _ = require("underscore");
+// this is the broker process for the host
+var fs = require("fs");
 var winston = require("winston");
 var express = require("express");
-var app = require("express")();
-var server = require("http").Server(app);
-var util = require(__dirname+"/lib/util");
-
-var debug = process.env.DEBUG ? true : false;
-
-var par = {
-	address: null, // set to 'localhost' for deployment
-	port: 8010,
-	mail_list: "robbinsdart@gmail.com",
-	log_path: __dirname + "/logs/",
-	send_emails: true
-};
+var http = require("http");
+var sockets = require("socket.io");
+var util = require("./lib/util");
+var par = JSON.parse(fs.readFileSync(__dirname + "/host-config.json"));
 
 var boxes = {};
 
-server.listen(par.port, par.address);
-
-var io = require('socket.io')(server);
-
-server.on('listening', function() {
-	console.log('Server listening on http://%s', util.getIPAddress());
+// server listens on two ports - one facing devices, one facing clients
+var app_dev = express();
+var serv_dev = http.Server(app_dev);
+serv_dev.listen(par.port_int, par.addr_int);
+serv_dev.on("listening", function() {
+    var addr = serv_dev.address();
+    winston.info("endpoint for devices: http://%s:%s", addr.address, addr.port);
 });
+var io_dev = sockets(serv_dev);
 
+var app_cli = express();
+var serv_cli = http.Server(app_cli);
+serv_cli.listen(par.port_ext, par.addr_ext);
+serv_cli.on("listening", function() {
+    var addr = serv_cli.address();
+    winston.info("endpoint for clients: http://%s:%s", addr.address, addr.port);
+});
+var io_cli = sockets(serv_cli);
 
 // channels for communicating with BBBs
-var bpub = io.of("/BPUB");
-var breq = io.of("/BREQ");
+var bpub = io_dev.of("/PUB");
+var breq = io_dev.of("/REQ");
 
 // channels for communicating with host-side clients
-var hpub = io.of("/HPUB");
-var hreq = io.of("/HREQ");
+var hpub = io_cli.of("/PUB");
+var hreq = io_cli.of("/REQ");
 
 // temporarily disabled for debugging
-// console.log("Starting baby-sitter");
-// var babysitter = require('child_process').fork(__dirname + "/baby-sitter.js");
+// winston.info("Starting baby-sitter");
+// var babysitter = require("child_process").fork(__dirname + "/baby-sitter.js");
 
+// handling PUB messages from devices
+bpub.on("connection", function(socket) {
+    var name = "unregistered";
 
-bpub.on('connection', function(socket) {
-	var name = "unregistered";
+    socket.emit("register-box", function(hostname, ip, port) {
+        name = hostname;
+        boxes[name] = {};
+        boxes[name].name = hostname;
+        boxes[name].ip = ip;
+        boxes[name].port = port;
+        boxes[name].socket = socket.id;
+        boxes[name].graceful_exit = false;
+        console.log(name, "connected");
+        hpub.emit("msg", {
+            addr: "",
+            time: Date.now(),
+            event: "box-registered",
+            data: boxes[name]
+        });
+        get_store(name);
+    });
 
-	socket.emit('register-box', function(hostname, ip, port) {
-		name = hostname;
-		boxes[name] = {};
-		boxes[name].name = hostname;
-		boxes[name].ip = ip;
-		boxes[name].port = port;
-		boxes[name].socket = socket.id;
-		boxes[name].graceful_exit = false;
-		console.log(name, "connected");
-		hpub.emit("msg", {
-			addr: "",
-			time: Date.now(),
-			event: "box-registered",
-			data: boxes[name]
-		});
-		get_store(name);
-	});
+    socket.on("msg", function(msg) {
+        // add the BBB hostname to the front of pub addresses
+        msg.addr = msg.addr ? name + "." + msg.addr : name;
 
-	socket.on('msg', function(msg) {
-		// add the BBB hostname to the front of pub addresses
-		msg.addr = msg.addr ? name + "." + msg.addr : name;
+        // forward BBB pub messages to the host-side clients
+        hpub.emit("msg", msg);
 
-		// forward BBB pub messages to the host-side clients
-		hpub.emit("msg", msg);
+        // log trial-data
+        // TODO: separate this into own component
+        if (msg.event == "trial-data") {
+            var datafile = msg.data.subject + "_" + msg.data.program + "_" + msg.data.box + ".json";
+            datalog.transports.file.filename = datalog.transports.file._basename = datafile;
 
-		// log trial-data
-		// TODO: separate this into own component
-		if (msg.event == "trial-data") {
-			var datafile = msg.data.subject + "_" + msg.data.program + "_" + msg.data.box + ".json";
-			datalog.transports.file.filename = datalog.transports.file._basename = datafile;
+            if (msg.data.end == 0) msg.error = "LOG ERROR: Trial terminated early";
 
-      if (msg.data.end == 0) msg.error = "LOG ERROR: Trial terminated early"; 
+            datalog.log("data", "data-logged", {
+                data: msg.data
+            });
+            console.log(name + ": data logged");
+        } else {
+            var eventfile = name + "_events.json";
+            eventlog.transports.file.filename = eventlog.transports.file._basename = eventfile;
 
-			datalog.log('data', "data-logged", {
-				data: msg.data
-			});
-			console.log(name + ": data logged");
-		} else {
-			var eventfile = name + "_events.json";
-			eventlog.transports.file.filename = eventlog.transports.file._basename = eventfile;
+            if (msg.event == "log") {
+                if (msg.level == "error") {
+                    eventlog.log("error", "error-logged", msg);
+                    console.log(name + ": error logged");
+                } else {
+                    eventlog.log("info", "info-logged", msg);
+                    console.log(name + ": info logged");
+                }
+            } else {
+                eventlog.log("event", "event-logged", msg);
+                console.log(name + ": event logged");
+            }
+        }
 
-			if (msg.event == "log") {
-				if (msg.level == "error") {
-					eventlog.log('error', "error-logged", msg);
-					console.log(name + ": error logged");
-				} else {
-					eventlog.log('info', "info-logged", msg);
-					console.log(name + ": info logged");
-				}
-			} else {
-				eventlog.log('event', "event-logged", msg);
-				console.log(name + ": event logged");
-			}
-		}
+        socket.on("graceful-exit", function() {
+            boxes[name].graceful_exit = true;
+        });
+    });
 
-		socket.on('graceful-exit', function() {
-			boxes[name].graceful_exit = true;
-		});
-	});
+    socket.on("disconnect", function() {
+        console.log(name, "disconnected");
+        if (boxes[name].graceful_exit === false) util.mail("host-server",par.mail_list,"Beaglebone disconnect", name + " disconnected from host - " + Date.now());
+        hpub.emit("msg", {
+            addr: "",
+            time: Date.now(),
+            event: "box-unregistered",
+            data: boxes[name]
+        });
+        delete boxes[name];
 
-	socket.on("disconnect", function() {
-		console.log(name, "disconnected");
-		if (boxes[name].graceful_exit === false) util.mail("host-server",par.mail_list,"Beaglebone disconnect", name + " disconnected from host - " + Date.now());
-		hpub.emit("msg", {
-			addr: "",
-			time: Date.now(),
-			event: "box-unregistered",
-			data: boxes[name]
-		});
-		delete boxes[name];
+    });
 
-	});
+    // set up unique loggers for each box
+    var datalog = new(winston.Logger)({
+        transports: [
+            new(winston.transports.File)({
+                filename: __dirname + par.log_path + "/data.json",
+                json: true,
+                timestamp: function() {
+                    return Date.now();
+                }
+            })
+        ]
+    });
+    datalog.levels.data = 3;
 
-	// set up unique loggers for each box
-	var datalog = new(winston.Logger)({
-		transports: [
-			new(winston.transports.File)({
-				filename: par.log_path + "data.json",
-				json: true,
-				timestamp: function() {
-					return Date.now();
-				}
-			})
-		]
-	});
-	datalog.levels.data = 3;
-
-	var eventlog = new(winston.Logger)({
-		transports: [
-			new(winston.transports.File)({
-				filename: par.log_path + "eventlog.json",
-				json: true,
-				timestamp: function() {
-					return Date.now();
-				}
-			})
-		]
-	});
-	eventlog.levels.event = 3;
+    var eventlog = new(winston.Logger)({
+        transports: [
+            new(winston.transports.File)({
+                filename: __dirname + par.log_path + "/eventlog.json",
+                json: true,
+                timestamp: function() {
+                    return Date.now();
+                }
+            })
+        ]
+    });
+    eventlog.levels.event = 3;
 }); // io.sockets.on
 
-if (!debug) {
-	process.on('uncaughtException', function(err) {
-		var subject = "Uncaught Exception";
-		var message = 'Caught exception: ' + err;
-		util.mail("host-server", par.mail_list, subject, message, process.exit);
-	});
+if (par.send_emails) {
+    process.on("uncaughtException", function(err) {
+        var subject = "Uncaught Exception";
+        var message = "Caught exception: " + err;
+        util.mail("host-server", par.mail_list, subject, message, process.exit);
+    });
 }
 
 function get_store(name) {
-	console.log("requesting " + name + " log store");
-	io.to(boxes[name].socket).emit("send-store");
+    console.log("requesting " + name + " log store");
+    io.to(boxes[name].socket).emit("send-store");
 }
 
 // handle http requests from clients
-app.get("/", function(req, res) {
-	res.sendfile(__dirname + "/static/directory.html");
+app_cli.get("/", function(req, res) {
+    res.sendfile(__dirname + "/static/directory.html");
 });
 
-app.get("/boxes", function(req, res) {
-	res.send(boxes);
+app_cli.get("/boxes", function(req, res) {
+    res.send(boxes);
 });
 
 // all other static content resides in /static
-app.use("/static", express.static(__dirname + "/static"));
+app_cli.use("/static", express.static(__dirname + "/static"));
