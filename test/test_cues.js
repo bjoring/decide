2756a0e0ccfdfa487649c42ac17438ea88100cd8
Dan Meliza <dan@meliza.org>
2014-08-04 13:01:43 -0400

    remove some excess code from alsa.js

--- test/test_cues.js
+++ test/test_cues.js
@@ -1,19 +1,20 @@
+var _ = require("underscore");
 var winston = require("winston");
 var cues = require("../lib/cues");
 
 winston.levels.pub = 3;
 winston.levels.req = 3;
 
-var cue = cues({device: "starboard:left:red"}, winston.pub);
+var cue = new cues({device: "starboard:left:red"}, _.partial(winston.log, "pub"));
 
-cue.req({req: "change-state", addr:"", data: { brightness: 255}}, winston.req);
+cue.req({req: "change-state", addr:"", data: { brightness: 255}},
+        _.partial(winston.log, "req"));
 
+cue.req({req: "change-state", addr:"",
+         data: { trigger: "timer", period: 500}},
+        _.partial(winston.log, "req"));
 
-// cues.subscribe("state-changed", console.log);
-// cues.subscribe("error", console.log);
-
-// cues.event({id: "modify-state", time: Date.now(), trigger: "timer", period: 500});
-
-// setTimeout(function() {
-//   cues.event({id: "modify-state", time: Date.now(), brightness: 0});
-// }, 15000);
+setTimeout(function() {
+    cue.req({req: "change-state", addr:"", data: { brightness: 0}},
+        _.partial(winston.log, "req"));
+}, 15000)
