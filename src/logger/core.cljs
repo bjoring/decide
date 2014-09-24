e1000fd74a4b15b08d6510d5861076b27d9dbed1
Dan Meliza <dan@meliza.org>
2014-09-24 15:57:23 -0400

    host logger now writes to disk

--- src/logger/core.cljs
+++ src/logger/core.cljs
@@ -1,27 +1,47 @@
 (ns decide.logger
   "Standalone logging program; runs on host"
-  (:require [cljs.nodejs :as node]))
+  (:require [cljs.nodejs :as node]
+            [clojure.string :as str]))
 
 (def version "1.0a2")
 
+(def fs (js/require "fs"))
 (def sock-cli (js/require "socket.io-client"))
-(def logger (js/require "../lib/log"))
+(def console (js/require "../lib/log"))
 
-(defn log [& args] (apply (.-log js/console) (map str args)))
+;; logging
+(defn- write-stream [path]
+  (.info console "opened" path "for logging")
+  (.createWriteStream fs path (clj->js {:flags "a" :encoding "utf-8"})))
+(defn- flatten-record [js & keys]
+  (let [js (js->clj js :keywordize-keys true)]
+    (merge (select-keys js keys) (:data js))))
 
+(def logfiles (atom {}))
+(defn write-record!
+  "Writes javascript [record] as serialized, line-delimited JSON to file at [path]"
+  [path record]
+  (let [*w* (or (get @logfiles path)
+                (get (swap! logfiles assoc path (write-stream path)) path))]
+    (.write *w* (str (JSON/stringify (clj->js record)) "\n"))))
+
+;; connect to host
 (def host (.connect sock-cli "http://localhost:8020" (clj->js {:transports ["websocket"]})))
-
-(.on host "connect" (fn [] (.info logger "connected to host")))
+(.on host "connect" (fn [] (.info console "connected to host")))
 (.on host "disconnect" (fn []
-                         (.error logger "lost connection to host")
+                         (.error console "lost connection to host")
                          (.exit node/process -1)))
 (.on host "state-changed"
      (fn [msg]
-       (.log logger "pub" "state-changed:" msg)))
+       (let [msg (flatten-record msg :time :addr)
+             logfile (str "events_" (first (str/split (:addr msg) #"\.")) ".jsonl")]
+         (write-record! logfile msg))))
 (.on host "trial-data"
      (fn [msg]
-       (.log logger "pub" "trial-data:" msg)))
+       (let [msg (flatten-record msg :time)
+             logfile (str (:subject msg) "_" (:program msg) ".jsonl")]
+         (write-record! logfile msg))))
 
 (defn- main [& args]
-  (.info logger "this is decide logger, version" version))
+  (.info console "this is decide console, version" version))
 (set! *main-cli-fn* main)
