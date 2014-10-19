edd60b2684754f5ca1b49eed13cb8416b0281e00
Dan Meliza <dan@meliza.org>
2014-10-19 12:11:38 -0400

    decide-ctrl resets apparatus when experiment disconnects

--- scripts/decide-ctrl.js
+++ scripts/decide-ctrl.js
@@ -131,7 +131,9 @@ io.on("connection", function(socket) {
             error("client " + client_key + " disconnected unexpectedly")
             apparatus.unregister(client_key);
             client_key = null;
-            // TODO: check if experiment needs to be reset
+            // reset the apparatus; TODO check if the client that disconnected
+            // was actually running the experiment!
+            apparatus.req("reset-state", "", null, function() {})
         }
         logger.info("disconnect from:", client_addr);
     });
