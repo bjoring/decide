7c9d659b11259f7036f43d959388903cdba7bd5d
Dan Meliza <dan@meliza.org>
2014-12-03 18:16:51 -0500

    add mail transport info to host-config

--- src/decide/core.cljs
+++ src/decide/core.cljs
@@ -6,12 +6,12 @@
 (def os (js/require "os"))
 (def path (js/require "path"))
 (def console (js/require "../lib/log"))
-(def mailer (.createTransport (js/require "nodemailer")))
 (def version (aget (js/require "../package.json") "version"))
 
 (defn config-path [name] (.resolve path js/__dirname ".." "config" name))
 (def config (json/read-json (config-path "host-config.json")))
 
+(def mailer (.createTransport (js/require "nodemailer") (:mail_transport config)))
 (defn mail [from to subject message]
   (.sendMail mailer (js-obj "from" (str from "@" (.hostname os))
                             "to" to
