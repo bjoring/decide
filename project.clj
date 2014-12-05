35752bfc60e9ce4f84edefbb867f06b4b296b2e8
Dan Meliza <dan@meliza.org>
2014-12-05 17:06:20 -0500

    tick version; remove async from project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject decide "1.0.0-beta.3"
+(defproject decide "1.0.0-beta.4"
   :description "operant control system"
   :url "http://meliza.org/starboard"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2173"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]]
+                 [org.clojure/clojurescript "0.0-2173"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
