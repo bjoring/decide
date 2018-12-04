3fe6ffdd47e89ddd16e92b544e9d0fc044e0157a
root <root@beaglebone.localdomain>
2018-12-03 20:17:56 -0500

    copy back in shell aplayer

--- test/test_sound.js
+++ test/test_sound.js
@@ -22,14 +22,33 @@
 const fs = require("fs");
 const wav = require("wav");
 const Speaker = require("speaker");
-let reader = new wav.Reader();
+let reader1 = new wav.Reader();
+let reader2 = new wav.Reader();
 let file1 = fs.createReadStream("stimuli/st15_ac3.wav");
 let file2 = fs.createReadStream("stimuli/st15_aq3.wav");
 
-file1.pipe(reader);
 
-
-reader.on('format', function(format) {
+reader1.on('format', function(format) {
     console.log(format);
-    reader.pipe(new Speaker(format));
+    let speaker = new Speaker(format);
+    speaker.once("flush", function() {
+        console.log("playing sound 2");
+        reader2.pipe(speaker);
+    })
+    reader1.pipe(speaker);
+    // reader2.on('format', function(format) {
+    //     reader1.pipe(speaker);
+    //     reader2.pipe(speaker);
+    // });
 });
+
+// reader1.on("close", function() {
+//     reader2.pipe(speaker);
+// })
+
+reader1.on("error", function(err) {
+    throw err;
+});
+
+file1.pipe(reader1);
+file2.pipe(reader2);
