41a5f5c3fa764375707faec903692897c4747471
root <root@beaglebone.localdomain>
2018-12-04 22:33:29 -0500

    total revamp of apparatus configuration
    bbb-config.json now used to specify drivers for each element.
    Should make swapping them out easier.

--- test/test_00A2.js
+++
@@ -1,6 +0,0 @@
-var b = require("../lib/bonescript-lite");
-// if cape not already loaded
-b.init_overlay("BBB-StarBoard", "00A2");
-// turn on center green led
-b.led_write("starboard:center:green", 1, console.log)
-
